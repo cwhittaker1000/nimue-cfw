9331503944ad097fc2e6f8f54c7f1be4425648f3
Charlie Whittaker <cwhittaker1000@gmail.com>
2025-12-02 11:03:35 -0800

    remove old version of nimue

--- src/registration.c
+++ src/registration.c
@@ -8,23 +8,12 @@
 */
 
 /* .C calls */
-extern void vaccine_initmod_desolve(void *);
-extern void vaccine_output_dde(void *);
-extern void vaccine_rhs_dde(void *);
-extern void vaccine_rhs_desolve(void *);
 extern void vaccine_simplified_initmod_desolve(void *);
 extern void vaccine_simplified_output_dde(void *);
 extern void vaccine_simplified_rhs_dde(void *);
 extern void vaccine_simplified_rhs_desolve(void *);
 
 /* .Call calls */
-extern SEXP vaccine_contents(SEXP);
-extern SEXP vaccine_create(SEXP);
-extern SEXP vaccine_initial_conditions(SEXP, SEXP);
-extern SEXP vaccine_metadata(SEXP);
-extern SEXP vaccine_rhs_r(SEXP, SEXP, SEXP);
-extern SEXP vaccine_set_initial(SEXP, SEXP, SEXP, SEXP);
-extern SEXP vaccine_set_user(SEXP, SEXP);
 extern SEXP vaccine_simplified_contents(SEXP);
 extern SEXP vaccine_simplified_create(SEXP);
 extern SEXP vaccine_simplified_initial_conditions(SEXP, SEXP);
@@ -34,10 +23,6 @@ extern SEXP vaccine_simplified_set_initial(SEXP, SEXP, SEXP, SEXP);
 extern SEXP vaccine_simplified_set_user(SEXP, SEXP);
 
 static const R_CMethodDef CEntries[] = {
-    {"vaccine_initmod_desolve",            (DL_FUNC) &vaccine_initmod_desolve,            1},
-    {"vaccine_output_dde",                 (DL_FUNC) &vaccine_output_dde,                 1},
-    {"vaccine_rhs_dde",                    (DL_FUNC) &vaccine_rhs_dde,                    1},
-    {"vaccine_rhs_desolve",                (DL_FUNC) &vaccine_rhs_desolve,                1},
     {"vaccine_simplified_initmod_desolve", (DL_FUNC) &vaccine_simplified_initmod_desolve, 1},
     {"vaccine_simplified_output_dde",      (DL_FUNC) &vaccine_simplified_output_dde,      1},
     {"vaccine_simplified_rhs_dde",         (DL_FUNC) &vaccine_simplified_rhs_dde,         1},
@@ -46,13 +31,6 @@ static const R_CMethodDef CEntries[] = {
 };
 
 static const R_CallMethodDef CallEntries[] = {
-    {"vaccine_contents",                      (DL_FUNC) &vaccine_contents,                      1},
-    {"vaccine_create",                        (DL_FUNC) &vaccine_create,                        1},
-    {"vaccine_initial_conditions",            (DL_FUNC) &vaccine_initial_conditions,            2},
-    {"vaccine_metadata",                      (DL_FUNC) &vaccine_metadata,                      1},
-    {"vaccine_rhs_r",                         (DL_FUNC) &vaccine_rhs_r,                         3},
-    {"vaccine_set_initial",                   (DL_FUNC) &vaccine_set_initial,                   4},
-    {"vaccine_set_user",                      (DL_FUNC) &vaccine_set_user,                      2},
     {"vaccine_simplified_contents",           (DL_FUNC) &vaccine_simplified_contents,           1},
     {"vaccine_simplified_create",             (DL_FUNC) &vaccine_simplified_create,             1},
     {"vaccine_simplified_initial_conditions", (DL_FUNC) &vaccine_simplified_initial_conditions, 2},
